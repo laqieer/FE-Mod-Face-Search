b1c2c4ae18bac37a1464508003d60204cf6702b3
laqieer <laqieer@126.com>
2022-04-27 00:25:57 +0800

    Process uploaded image's dimension

--- app.js
+++ app.js
@@ -18,16 +18,32 @@ class App {
     async doUpload( event ) {
         this.file = event.target.files[0];
         const bitmap = await createImageBitmap( this.file );
+        if ( bitmap.width <= 64 || bitmap.height <= 64 ) {
+            alert("Image dimension is too small!");
+            return;
+        }
+        if ( bitmap.width > 160 || bitmap.height > 160 ) {
+            alert("Image dimension is too big!");
+            return;
+        }
         this.preview.width = bitmap.width;
         this.preview.height = bitmap.height;
         var ctx = this.preview.getContext("2d");
+        if ( this.preview.width == 160 && this.preview.height == 160 ) {
+            this.preview.width = 128;
+            this.preview.height = 128;
+        }
+        if ( this.preview.width == 128 && this.preview.height == 112 ) {
+            this.preview.width = 96;
+            this.preview.height = 80;
+        }
         ctx.drawImage( bitmap, 0, 0 );
-        var imgData = ctx.getImageData( 0, 0, bitmap.width, bitmap.height );
+        var imgData = ctx.getImageData( 0, 0, this.preview.width, this.preview.height );
         var hash = bmvbhash( imgData, 8 );
         console.log( 'Hash: ' + hash );
         var imgs = this.refMap[hash];
         if ( imgs == undefined ) {
-            imgs = []
+            imgs = [];
         }
         this.result.innerHTML = '<b>' + imgs.length + ' results found.' + '</b>';
         for ( var i in imgs ) {
